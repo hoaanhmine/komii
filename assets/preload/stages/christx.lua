1befd126dd10bf65a4d1185703a97128f133935d
hoaanhmine <hoaminecraftpc@gmail.com>
2023-03-09 09:38:10 +0700

    done

---
+++ assets/preload/stages/christx.lua
@@ -0,0 +1,148 @@
+--JesusLight
+
+stagePath = 'Stages/AmTake/christ/'
+
+function onCreate()
+
+    makeLuaSprite('bgMall', 'Stages/AmTake/christ/mall_back', -1100, -700)
+    setScrollFactor('bgMall', 0.1, 0.1)
+    addLuaSprite('bgMall', true)
+
+end
+
+function onCreatePost()
+
+    makeLuaSprite('jesus', 'JesusLight', 0, 0)
+    setObjectCamera('jesus', 'hud')
+    setObjectOrder('jesus', '1')
+    setProperty('jesus.y', -200)
+    scaleObject('jesus', 0.39, 0.39)
+    screenCenter('jesus', 'x')
+    setBlendMode('jesus', 'multiply')
+    addLuaSprite('jesus')
+ 
+
+end
+
+function onCreate()
+    makeLuaSprite('mall_back', 'christ/images/mall_back', -1100, -700);
+	setScrollFactor('mall_back', 0.1, 0.1);
+	scaleObject('mall_back',1, 1)
+	addLuaSprite('mall_back', false);
+	
+	makeLuaSprite('TheWindow', 'christ/images/TheWindow', -815, -290);
+	setScrollFactor('TheWindow', 0.2, 0.2);
+	scaleObject('TheWindow',1, 1)
+	addLuaSprite('TheWindow', false);
+    
+    makeAnimatedLuaSprite('boppers', 'christ/images/cocnew/boppers', -675, -290);
+    setLuaSpriteScrollFactor('boppers', 0.31, 0.31);		
+	scaleObject('boppers',1, 1)
+    addAnimationByPrefix('boppers','idle', 'BG DUMBS',24,true);
+	addLuaSprite('boppers', false);
+
+    makeLuaSprite('MALL_BG_ESCALATORSSESES', 'christ/images/MALL_BG_ESCALATORSSESES', -750, -160);
+	setScrollFactor('MALL_BG_ESCALATORSSESES', 0.3, 0.3);
+	scaleObject('MALL_BG_ESCALATORSSESES',1, 1)
+	addLuaSprite('MALL_BG_ESCALATORSSESES', false);
+	
+	makeAnimatedLuaSprite('boppers1', 'christ/images/cocnew/boppers', -480, 480);
+    setLuaSpriteScrollFactor('boppers1', 0.3, 0.6);		
+	scaleObject('boppers1',1, 1)
+    addAnimationByPrefix('boppers1','idle', 'Gays',24,true);
+	addLuaSprite('boppers1', false);
+	
+	makeAnimatedLuaSprite('boppers2', 'christ/images/cocnew/boppers',1650, 440);
+    setLuaSpriteScrollFactor('boppers2', 0.4, 0.4);		
+	scaleObject('boppers2',1, 1)
+    addAnimationByPrefix('boppers2','idle', 'juz',24,true);
+	addLuaSprite('boppers2', false);
+	
+	makeAnimatedLuaSprite('boppers3', 'christ/images/cocnew/boppers',-620, 400);
+    setLuaSpriteScrollFactor('boppers3', 0.5, 0.6);		
+	scaleObject('boppers3',1, 1)
+    addAnimationByPrefix('boppers3','idle', 'leaked guy',24,true);
+	addLuaSprite('boppers3', false);
+
+    makeLuaSprite('Snow', 'christ/images/Snow', -900, 200);
+	setScrollFactor('Snow', 1, 1);
+	scaleObject('Snow',1.2, 1.2)
+	addLuaSprite('Snow', false);
+	
+	makeLuaSprite('TREEXDSXD', 'christ/images/TREEXDSXD', 330, -250);
+	setScrollFactor('TREEXDSXD', 0.95, 0.95);
+	scaleObject('TREEXDSXD',1, 1)
+	addLuaSprite('TREEXDSXD', false);
+	
+	makeAnimatedLuaSprite('boppers4', 'christ/images/cocnew/boppers',-790, 400);
+    setLuaSpriteScrollFactor('boppers4', 1, 1);		
+	scaleObject('boppers4',1, 1)
+    addAnimationByPrefix('boppers4','idle', 'Cass',24,true);
+	addLuaSprite('boppers4', false);
+	
+	makeAnimatedLuaSprite('boppers5', 'christ/images/cocnew/boppers',1420, 360);
+    setLuaSpriteScrollFactor('boppers5', 1, 1);		
+	scaleObject('boppers5',1, 1)
+    addAnimationByPrefix('boppers5','idle', 'zona',24,true);
+	addLuaSprite('boppers5', false);
+	
+	makeAnimatedLuaSprite('boppers6', 'christ/images/SmoothCC',120, -190);
+    setLuaSpriteScrollFactor('boppers6', 1, 1);		
+	scaleObject('boppers6',1, 1)
+    addAnimationByPrefix('boppers6','idle', 'castle crashers',24,true);
+	addLuaSprite('boppers6', false);
+	
+	makeLuaSprite('Snowdick', 'christ/images/Snowdick', 1460, 940);
+	setScrollFactor('Snowdick', 1, 1);
+	scaleObject('Snowdick',1.1, 1.1)
+	addLuaSprite('Snowdick', false);
+	
+	makeAnimatedLuaSprite('snowfallin_bg', 'christ/images/snowfallin_bg', -950, -470);
+    setLuaSpriteScrollFactor('snowfallin_bg', 1.15, 1.15);		
+	scaleObject('snowfallin_bg',2.4, 2.4)
+    addAnimationByPrefix('snowfallin_bg','idle', 'snowfallin',24,true);
+	addLuaSprite('snowfallin_bg', true);
+	
+	makeAnimatedLuaSprite('santa', 'christ/images/santa', -820, 840);
+    setLuaSpriteScrollFactor('santa', 1.5, 1.4);		
+	scaleObject('santa',0.9, 0.9)
+    addAnimationByPrefix('santa','idle', 'santa',24,true);
+	addLuaSprite('santa', true);
+
+    makeLuaSprite('cinemadeez1')
+    makeLuaSprite('cinemadeez2')
+    makeGraphic('cinemadeez1',  1500, 40, '000000')
+    makeGraphic('cinemadeez2', 1500, 40, '000000')
+    addLuaSprite('cinemadeez1', true)
+    addLuaSprite('cinemadeez2', true)
+    setObjectCamera('cinemadeez1', 'hud')
+    setObjectCamera('cinemadeez2', 'hud')
+    setProperty('cinemadeez1.y', 720)
+    setProperty('cinemadeez2.y', -40)
+    setObjectOrder('cinemadeez1', 1)
+    setObjectOrder('cinemadeez2', 0)
+
+end
+function onSongStart()
+    doTweenY('cinemadeez1Y', 'cinemadeez1', 690, 2.5, 'circOut')
+    doTweenY('cinemadeez2Y', 'cinemadeez2', -10, 2.5, 'circOut')
+end
+
+function onBeatHit()
+
+    if songName == 'Eggnog' and curBeat == 184 then
+        doTweenY('cinemadeez1Y', 'cinemadeez1', 720, 1.5, 'circOut')
+        doTweenY('cinemadeez2Y', 'cinemadeez2', -40, 1.5, 'circOut')
+    elseif songName == 'Eggnog' and curBeat == 216 then
+        doTweenY('cinemadeez1Y', 'cinemadeez1', 690, 2.5, 'circOut')
+        doTweenY('cinemadeez2Y', 'cinemadeez2', -10, 2.5, 'circOut')
+    end
+
+    if songName == 'Cocoa' and curBeat == 128 then
+        doTweenY('cinemadeez1Y', 'cinemadeez1', 720, 1.5, 'circOut')
+        doTweenY('cinemadeez2Y', 'cinemadeez2', -40, 1.5, 'circOut')
+    elseif songName == 'Cocoa' and curBeat == 160 then
+        doTweenY('cinemadeez1Y', 'cinemadeez1', 690, 2.5, 'circOut')
+        doTweenY('cinemadeez2Y', 'cinemadeez2', -10, 2.5, 'circOut')
+    end
+end
